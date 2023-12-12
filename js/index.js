aa1c0311b8e36c5f8bdc280e6fd930097bd4e21c
Facundo Pereira <pereirafacundo66@gmail.com>
2023-12-11 21:50:27 -0300

    cambios

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-console.log("hola")
+let viewMore = document.getElementsByClassName("view-more")
+
+
+function mostrarMas(){
+    let btnViewMore = document.getElementById("view-more")
+    for(i=0; i<viewMore.length; i++){
+        if(window.getComputedStyle(viewMore[i]).display === "none"){
+            viewMore[i].style.display = "block"
+            btnViewMore.textContent = "Ver menos"
+        }
+
+        else if(window.getComputedStyle(viewMore[i]).display === "block"){
+            viewMore[i].style.display = "none"
+            btnViewMore.textContent = "Ver más"
+        }
+    }
+}
+
+
+function fullScreen(event) {
+    let imagenClickeada = event.target
+    imagenClickeada.classList.toggle('imagen-fullscreen');
+    console.log(imagenClickeada)
+}
